39eb1a3f9d11f855f45ba0a473f4598ad34989e5
matt11matthew <matt11matthew@gmail.com>
2024-06-14 12:37:15 -0400

    updating

--- src/main/java/me/matthewe/dungeonrealms/drenhanced/dungeonrealmsenhanced/DREnhanced.java
+++ src/main/java/me/matthewe/dungeonrealms/drenhanced/dungeonrealmsenhanced/DREnhanced.java
@@ -48,7 +48,7 @@ public class DREnhanced {
     public static IProxy proxy;
     public static final String MOD_ID = "dungeonrealmsenhanced";
     public static final String MOD_NAME = "DREnhanced";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.1";
     public static GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting()
             .disableHtmlEscaping()
             .registerTypeAdapter(Changelog.class, new ChangelogJsonSerializer())

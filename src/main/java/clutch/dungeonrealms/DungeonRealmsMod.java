cbf74fc1f90aa326cd876cc4d8949e3de868ed19
matt11matthew <matt11matthew@gmail.com>
2025-02-27 21:16:14 -0500

    Update DungeonRealmsMod.java

--- src/main/java/clutch/dungeonrealms/DungeonRealmsMod.java
+++ src/main/java/clutch/dungeonrealms/DungeonRealmsMod.java
@@ -1,12 +1,10 @@
 package clutch.dungeonrealms;
 
-import net.minecraftforge.common.MinecraftForge;
-
 public class DungeonRealmsMod {
 
 
     public static void init(){
-        MinecraftForge.EVENT_BUS.register(new RenderOverlay());
+//        MinecraftForge.EVENT_BUS.register(new RenderOverlay());
 //        MinecraftForge.EVENT_BUS.register(new ArmorTooltipCompare());
     }
 }
